09c9b17a62871e8bf84a72a50e56dbaa9f6606eb
Thierry Degeling <thierry.degeling@gmail.com>
2013-11-26 19:16:55 +0100

    Time headers are now sticky ;) fixes #1

--- src/web/js/bundle-bundle_application_defer.js
+++ src/web/js/bundle-bundle_application_defer.js
@@ -53,3 +53,65 @@
       }, 1000);
    });
 }(window.jQuery);
+
+function stickyHeaders(stickies) {
+
+    this.load = function() {
+
+        stickies.each(function(){
+
+            var thisSticky = jQuery(this).wrap('<div class="sticky-header" />');
+            thisSticky.parent().height(thisSticky.outerHeight());
+
+            jQuery.data(thisSticky[0], 'pos', thisSticky.offset().top);
+
+        });
+    }
+
+    this.scroll = function() {
+
+        stickies.each(function(i){
+
+
+            var thisSticky = jQuery(this),
+                nextSticky = stickies.eq(i+1),
+                prevSticky = stickies.eq(i-1),
+                pos = jQuery.data(thisSticky[0], 'pos');
+
+            if (pos <= ( jQuery(window).scrollTop() + 50 )) {
+
+                thisSticky.addClass("fixed");
+
+                if (nextSticky.length > 0 && thisSticky.offset().top >= jQuery.data(nextSticky[0], 'pos') - ( thisSticky.outerHeight() + 50 ) ) {
+
+                    thisSticky.addClass("absolute").css("top", jQuery.data(nextSticky[0], 'pos') - thisSticky.outerHeight());
+
+                }
+
+            } else {
+
+                thisSticky.removeClass("fixed");
+
+                if (prevSticky.length > 0 && jQuery(window).scrollTop() <= jQuery.data(thisSticky[0], 'pos')  - ( prevSticky.outerHeight() + 50) ) {
+
+                    prevSticky.removeClass("absolute").removeAttr("style");
+
+                }
+
+            }
+        });
+    }
+}
+
+jQuery(document).ready(function(){
+
+    var newStickies = new stickyHeaders(jQuery(".sticky-header"));
+
+    newStickies.load();
+
+    jQuery(window).on("scroll", function() {
+
+        newStickies.scroll();
+
+    });
+});
