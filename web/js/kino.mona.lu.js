1fd3dfea763afa9bded7b2ef4e0b1ffacb7793ac
Thierry Degeling <thierry.degeling@gmail.com>
2014-11-25 10:40:45 +0000

    not in use anymore

--- web/js/kino.mona.lu.js
+++ web/js/kino.mona.lu.js
@@ -2,27 +2,6 @@
 
     $(function(){
 
-        $(document).ready(function() {
-            $(".ellipsis").dotdotdot();
-        });
-
-        $('.screeningTime').each(function(idx){
-            $(this).html(
-                moment.unix(
-                    $(this).html()
-                ).format(
-                    "HH"
-                )
-                + "<span class=\"minutes\">"
-                + moment.unix(
-                    $(this).html()
-                ).format(
-                    "mm"
-                )
-                + "</span>"
-            );
-        });
-
     });
 
     $( '.movie' ).on( "click", function() {
