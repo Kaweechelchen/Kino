6d4a957fc0bbd0f6c8448527ad881e4ff8ec6129
Thierry Degeling <thierry@syn2cat.lu>
2017-07-14 10:34:59 +0200

    cleanup

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -19,52 +19,31 @@ class ScrapeController extends Controller
         app('Kinepolis')->scrape();
 
         foreach (app('Kinepolis')->genres() as $genreId => $genre) {
-            Genre::updateOrCreate(
-                ['id' => $genreId],
-                ['genre' => $genre]
-            );
+            Genre::updateOrCreate(['id' => $genreId], ['genre' => $genre]);
         }
 
         foreach (app('Kinepolis')->languages() as $languageId => $language) {
-            Language::updateOrCreate(
-                ['id' => $languageId],
-                ['language' => $language]
-            );
+            Language::updateOrCreate(['id' => $languageId], ['language' => $language]);
         }
 
         foreach (app('Kinepolis')->categories() as $categoryId => $category) {
-            Category::updateOrCreate(
-                ['id' => $categoryId],
-                ['category' => $category]
-            );
+            Category::updateOrCreate(['id' => $categoryId], ['category' => $category]);
         }
 
         foreach (app('Kinepolis')->formats() as $formatId => $format) {
-            Format::updateOrCreate(
-                ['id' => $formatId],
-                ['format' => $format]
-            );
+            Format::updateOrCreate(['id' => $formatId], ['format' => $format]);
         }
 
         foreach (app('Kinepolis')->types() as $typeId => $type) {
-            Type::updateOrCreate(
-                ['id' => $typeId],
-                ['type' => $type]
-            );
+            Type::updateOrCreate(['id' => $typeId], ['type' => $type]);
         }
 
         foreach (app('Kinepolis')->countries() as $countryId => $country) {
-            Country::updateOrCreate(
-                ['id' => $countryId],
-                ['country' => $country]
-            );
+            Country::updateOrCreate(['id' => $countryId], ['country' => $country]);
         }
 
         foreach (app('Kinepolis')->theatres() as $theatreId => $theatre) {
-            Theatre::updateOrCreate(
-                ['id' => $theatreId],
-                ['theatre' => $theatre]
-            );
+            Theatre::updateOrCreate(['id' => $theatreId], ['theatre' => $theatre]);
         }
 
         dd(app('Kinepolis')->genres());
