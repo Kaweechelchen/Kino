978080550d72cb4474fd43deccf96a9073d4a0cb
Thierry Degeling <thierry@syn2cat.lu>
2017-07-16 12:19:35 +0200

    optimization for small devices

--- resources/assets/js/components/Movie.vue
+++ resources/assets/js/components/Movie.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="MovieContainer">
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-xs-2">
                 <img :src="poster(movie)">
             </div>
-            <div class="col-sm-10 metaData">
+            <div class="col-xs-10 metaData">
                 <theatre
                     v-for="(versions, theatre) in theatres"
                     :theatre="theatre"
