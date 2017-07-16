33b2f73767be4a4e970676d24214da24bbff58f9
Thierry Degeling <thierry@syn2cat.lu>
2017-07-16 14:55:12 +0200

    meta tags & logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,36 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Tezza (https://twitter.com/FAQ)">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Movies &middot; Morroni @yield('title')</title>
+    <meta name="description" content="Movie Showtimes by Morroni Studios">
+    <meta property="og:title" content="Movie Showtimes by Morroni Studios" />
+    <meta property="og:description" content="Upcoming movie screenings in theatres of Luxembourg" />
+    <meta property="og:image" content="{{ URL::to('/') }}/img/logo.svg" />
+    <link rel="shortcut icon" href="{{ URL::to('/') }}/img/logo.svg" />
+    <title>Movies &middot; Morroni Studios</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="@yield('bodyClass')">
-
     <div class="container-fluid" id="app">
         @yield('content')
     </div>
-
     @include('footer')
-
     <script src="{{ mix('/js/app.js') }}"></script>
-    <script>
-        /*(function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', '{{ env('GOOGLE_ANALYTICS') }}', 'auto');
-        ga('send', 'pageview');*/
-    </script>
 </body>
 
 </html>
