935dc2b3b5b4974312bdd06c903ce0af92cd6af5
Thierry Degeling <thierry@syn2cat.lu>
2017-07-16 12:08:39 +0200

    font awesome integration

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="center">
-    <i title="coded" class="fa fa-code" aria-hidden="true"></i> with <i title="love" class="fa fa-heart red pulse" aria-hidden="true"></i> by <a href="/about">Tezza</a>
+    <i title="coded" class="fa fa-code" aria-hidden="true"></i> with <i title="love" class="fa fa-heart red pulse" aria-hidden="true"></i> by <a href="https://twitter.com/FAQ">Tezza</a>
 </footer>
